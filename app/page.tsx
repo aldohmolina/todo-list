bfc08f5176258ececb6628b5872c31de47cf36f3
aldoximple <aldo_hernandez@ximple.co>
2024-08-21 18:34:36 -0600

    adding login & sessions

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,50 @@
 import { getTodos } from "@/actions";
 import { MainPage } from "@/components";
 
+const navLinks = [{ title: "Home", href: "/", badge: 0 }];
+import Link from "next/link";
+import { PackageIcon } from "@/components/ui/icons";
+import { Badge } from "lucide-react";
+import LogoutButton from "./logout-button";
+
 export default async function Home() {
   const todos = (await getTodos()) ?? [];
-
+  const activeLink = "/";
   return (
     <main className="flex min-h-screen flex-col items-center p-5">
+      <div className="flex h-full w-full gap-2 mb-10">
+        <div className="flex h-14 items-center border-b px-4">
+          <Link className="flex items-center gap-2 font-semibold" href="#">
+            <PackageIcon className="h-6 w-6" />
+            <span className="">Acme Inc</span>
+          </Link>
+        </div>
+        <div className="flex-1 overflow-auto py-2">
+          <nav className="grid items-start px-4 text-sm font-medium">
+            {navLinks.map((link) => (
+              <Link
+                className={`${
+                  activeLink === link.href
+                    ? "bg-gray-100 text-gray-900 hover:text-gray-900"
+                    : ""
+                } flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900`}
+                href={link.href}
+                key={link.title}
+              >
+                <span>{link.title}</span>
+                {link.badge > 0 && (
+                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    {link.badge}
+                  </Badge>
+                )}
+              </Link>
+            ))}
+          </nav>
+        </div>
+        <div className="border-t p-4">
+          <LogoutButton />
+        </div>
+      </div>
       <MainPage todos={todos} />
     </main>
   );
---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "@/app/auth/02-stateless-session";
+import { cookies } from "next/headers";
+
+// 1. Specify protected and public routes
+const protectedRoutes = ["/"];
+const publicRoutes = ["/login", "/signup"];
+
+export default async function middleware(req: NextRequest) {
+  // 2. Check if the current route is protected or public
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
+  // 3. Decrypt the session from the cookie
+  const cookie = cookies().get("session")?.value;
+  const session = await decrypt(cookie);
+
+  // 4. Redirect
+  if (isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+
+  if (
+    isPublicRoute &&
+    session?.userId &&
+    !req.nextUrl.pathname.startsWith("/")
+  ) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
